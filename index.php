013550b92a68aaa8eb01335a24029e53e1f9ff6d
saman <saman.ramezanii.20@gmail.com>
2023-05-13 15:54:48 -0700

    نوشتن اضافه کردن در ایندکس

--- index.php
+++ index.php
@@ -12,12 +12,20 @@ if (isset($_POST['submit'])){
     $classid=$_POST['txt_classid'];
     $classname=$_POST['txt_class'];
     $sql="INSERT INTO class(class_id,class_anme) VALUES ('$classid','classname')";
-    $sql_pre=$db->prepare($query);
+    $sql_pre=$db->prepare($sql);
     $sql_pre->execute();
 }
 if (isset($_POST['submit1'])){
-    $classid=$_POST
+    $classid=$_POST['classid'];
+    $stud_id=$_POST['studid'];
+    $stud_name=$_POST['stud_name'];
+    $stud_family=$_POST['stud_family'];
+    $stud_ave=$_POST['stud_ave'];
+    $sql="INSERT INTO student(stud_id,class_id,name,family,ave) VALUES ('$stud_id','$classid','$stud_name','$stud_family','$stud_ave')";
+    $sql_pre=$db->prepare($sql);
+    $sql_pre->execute();
 }
+
 ?>
 </body>
 </html>

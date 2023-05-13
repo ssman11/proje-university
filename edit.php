1484fc6ee550dc84fac472c67752b6d591bd4c3a
saman <saman.ramezanii.20@gmail.com>
2023-05-13 13:26:54 -0700

    درحال تکمیل سویچ

--- edit.php
+++ edit.php
@@ -6,6 +6,24 @@
     </head>
     <body>
     <?php session_start(); ob_start();
+    case 2:
+        include "connection.php";
+        $query= "SELECT * FROM student WHERE stud_id=". $id;
+        $result=$db->prepare($query);
+        $result->execute();
+        $row= $result->fetch(PDO::FETCH_ASSOC);
+        if (isset($_POST['submit1'])) {
+            $name=$_POST['stud_name'];
+            $family=$_POST['stud_family'];
+            $ave=$_POST['stud_ave'];
+            $studid=$_POST['stud_id'];
+            $classid=$_POST['class_id'];
+            $query="UPDATE student SET name='".$name."'".",family='" . $family . "'" . ",ave=" . $ave . ",stud_id='" . $studid . "'" . " ,class_id='" . $classid . "'" . " WHERE stud_id=" . $id;
+            $edit = $db->prepare($query);
+            $edit->execute();
+            header("location:index.php");
+            
+        }
     include "connection.php";
     if (isset($_GET['id']) && isset($_GET['page']) ) {
         $id= $_GET['id'];
@@ -24,7 +42,23 @@
                 $del->execute();
                 header("location:index.php");
             }
+            echo ' <div id="row1_col1">
+    <form action="" method="post">
+        <fieldset>
+            <legend>class</legend>
+                    <label> class</label>
+    <input type="text" name="txt_class" value="' . $row['class_name'] . '"/>
+                            <label> class<label>
+                                    <input type="text" name="txt_class1" value="' . $row['class_id'] . '"/>
+                                    <input type="submit" name="submit" value="edit-class"/>
+        </fieldset>
+    </form>
+    </div>
+    break;
+    
+            
         }
     }
     </body>
     </html>
+    

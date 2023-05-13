abc69b1fcbbc9b860740051cf547f017c5dc1398
saman <saman.ramezanii.20@gmail.com>
2023-05-13 13:42:37 -0700

    مرتب کردن با کنترل الت و ال

--- delete.php
+++ delete.php
@@ -1,1 +1,10 @@
-<?php
+<?php session_start(); ob_start();
+include "connection.php";
+if (isset($_GET['id']) && isset($_GET['page']) ){
+  $id=$_GET['id'];
+  $page=$_GET['id'];
+  switch ($page){
+      case 1:
+          $query "d"
+  }
+}
--- edit.php
+++ edit.php
@@ -5,26 +5,28 @@
         <title>update</title>
     </head>
     <body>
-    <?php session_start(); ob_start();
+    <?php session_start();
+    ob_start();
     include "connection.php";
-    if (isset($_GET['id']) && isset($_GET['page']) ) {
-        $id= $_GET['id'];
-        $page=$_GET['id'];
+    if (isset($_GET['id']) && isset($_GET['page'])) {
+        $id = $_GET['id'];
+        $page = $_GET['id'];
         switch ($page) {
-            case 1: include "connection.php";
-            $query= "SELECT * FROM class WHERE class_id=".$id;
-            $result= $db->prepare($query);
-            $result->execute();
-            $row =$result->fetch(PDO::FETCH_ASSOC);
-            if (isset($_POST['submit'])) {
-                $class_name =$_POST['txt_class'];
-                $class_id = $_POST['txt_class1'];
-                $query="UPDATE class SET class_name='" . $class_name . "',class_id='" . $class_id ."' WHERE class_id=" . $id;
-                $del= $db->prepare($query);
-                $del->execute();
-                header("location:index.php");
-            }
-            echo ' <div id="row1_col1">
+            case 1:
+                include "connection.php";
+                $query = "SELECT * FROM class WHERE class_id=" . $id;
+                $result = $db->prepare($query);
+                $result->execute();
+                $row = $result->fetch(PDO::FETCH_ASSOC);
+                if (isset($_POST['submit'])) {
+                    $class_name = $_POST['txt_class'];
+                    $class_id = $_POST['txt_class1'];
+                    $query = "UPDATE class SET class_name='" . $class_name . "',class_id='" . $class_id . "' WHERE class_id=" . $id;
+                    $del = $db->prepare($query);
+                    $del->execute();
+                    header("location:index.php");
+                }
+                echo ' <div id="row1_col1">
     <form action="" method="post">
         <fieldset>
             <legend>class</legend>
@@ -36,26 +38,26 @@
         </fieldset>
     </form>
     </div>';
-    break;
-    case 2:
-        include "connection.php";
-        $query= "SELECT * FROM student WHERE stud_id=". $id;
-        $result=$db->prepare($query);
-        $result->execute();
-        $row= $result->fetch(PDO::FETCH_ASSOC);
-        if (isset($_POST['submit1'])) {
-            $name=$_POST['stud_name'];
-            $family=$_POST['stud_family'];
-            $ave=$_POST['stud_ave'];
-            $studid=$_POST['stud_id'];
-            $classid=$_POST['class_id'];
-            $query="UPDATE student SET name='".$name."'".",family='" . $family . "'" . ",ave=" . $ave . ",stud_id='" . $studid . "'" . " ,class_id='" . $classid . "'" . " WHERE stud_id=" . $id;
-            $edit = $db->prepare($query);
-            $edit->execute();
-            header("location:index.php");
+                break;
+            case 2:
+                include "connection.php";
+                $query = "SELECT * FROM student WHERE stud_id=" . $id;
+                $result = $db->prepare($query);
+                $result->execute();
+                $row = $result->fetch(PDO::FETCH_ASSOC);
+                if (isset($_POST['submit1'])) {
+                    $name = $_POST['stud_name'];
+                    $family = $_POST['stud_family'];
+                    $ave = $_POST['stud_ave'];
+                    $studid = $_POST['stud_id'];
+                    $classid = $_POST['class_id'];
+                    $query = "UPDATE student SET name='" . $name . "'" . ",family='" . $family . "'" . ",ave=" . $ave . ",stud_id='" . $studid . "'" . " ,class_id='" . $classid . "'" . " WHERE stud_id=" . $id;
+                    $edit = $db->prepare($query);
+                    $edit->execute();
+                    header("location:index.php");
 
-        }
-        echo '<div id="row1_col2">
+                }
+                echo '<div id="row1_col2">
         <form action="" method="post">
             <fieldest>
                 <legend>Student</legend>
@@ -74,8 +76,8 @@
         </form>
     </div>
     ';
-    
-            
+
+
         }
     }
     ?>

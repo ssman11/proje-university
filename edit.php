a36fa4bc24ac2bdcfeab82ed161afd758842b80e
saman <saman.ramezanii.20@gmail.com>
2023-05-13 12:45:46 -0700

    در حال اضافه کردن اپدیت

--- edit.php
+++ edit.php
@@ -1,1 +1,12 @@
-<?php
+<doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>update</title>
+    </head>
+    <body>
+    <?php session_start(); ob_start();
+    include "connection.php";
+    if (isset($_POST['']))
+    </body>
+    </html>

e295737bc3e6af603fc7d7faf36861303f8c923d
saman <saman.ramezanii.20@gmail.com>
2023-05-13 13:48:25 -0700

    تکمیل حذف و مرتب کردن نوشته ها

--- delete.php
+++ delete.php
@@ -1,15 +1,21 @@
-<?php session_start(); ob_start();
+<?php session_start();
+ob_start();
 include "connection.php";
-if (isset($_GET['id']) && isset($_GET['page']) ){
-  $id=$_GET['id'];
-  $page=$_GET['id'];
-  switch ($page){
-      case 1:
-          $query ="DELETE FROM class WHERE class_id=" .$id;
-          $del=$db->prepare($query);
-          $del->execute();
-          header("location:index.php");
-          break;
-          
-  }
+if (isset($_GET['id']) && isset($_GET['page'])) {
+    $id = $_GET['id'];
+    $page = $_GET['id'];
+    switch ($page) {
+        case 1:
+            $query = "DELETE FROM class WHERE class_id=" . $id;
+            $del = $db->prepare($query);
+            $del->execute();
+            header("location:index.php");
+            break;
+        case 2:
+            $query = "DELETE FROM student WHERE stud_id=" . $id;
+            $del = $db->prepare($query);
+            $del->execute();
+            header("location:index.php");
+            break;
+    }
 }

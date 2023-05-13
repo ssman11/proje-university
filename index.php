8160cf0c72d73419ecbc7fdff65dafcd060a6f67
saman <saman.ramezanii.20@gmail.com>
2023-05-13 15:46:15 -0700

    شروع کردن ایندکس

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>university database</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
 <?php
+include "connection.php";
+if (isset($_POST['submit'])){
+    $classid=$_POST['txt_classid'];
+    $classname=$_POST['txt_class'];
+    $sql="INSERT INTO class(class_id,class_anme) VALUES ('$classid','classname')";
+    $sql_pre=$db->prepare($query);
+    $sql_pre->execute();
+}
+if (isset($_POST['submit1'])){
+    $classid=$_POST
+}
+?>
+</body>
+</html>
+
+

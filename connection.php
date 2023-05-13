b688696c70da202c8602f69c686fd5645ce13d4d
saman <saman.ramezanii.20@gmail.com>
2023-05-13 12:33:53 -0700

    اضافه کردن کانکشن

--- connection.php
+++ connection.php
@@ -1,1 +1,11 @@
 <?php
+$host = "local host";
+$user = "root";
+$pass = "";
+$dbname = "university";
+
+
+$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$sql = "SET NAMES 'utf8'";
+$result = $db->prepare($sql);
+$result->execute();
